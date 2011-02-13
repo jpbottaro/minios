59ed3b16b9adcc10d228def19bc3814d7756ee89
Juan Pablo Bottaro <jpbottaro@gmail.com>
2011-02-13 17:55:55 -0300

    part of last commit I forgot
    this belongs to commit 3e50fc04fabd583fe2fcb6e3e183d6867eaa7773

--- kernel/sched.c
+++ kernel/sched.c
@@ -10,3 +10,13 @@ void schedule()
 void add_process(char *path)
 {
 }
+
+unsigned int current_uid()
+{
+    return ps[current_process].uid;
+}
+
+unsigned int current_gid()
+{
+    return ps[current_process].gid;
+}
