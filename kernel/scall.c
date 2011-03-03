4ec8436204b680ebf7c2bf8ec4ff0a877027ef24
Juan Pablo Bottaro <jpbottaro@gmail.com>
2011-03-03 20:15:53 -0300

    kernel: disable non-coded sys calls

--- kernel/scall.c
+++ kernel/scall.c
@@ -31,18 +31,18 @@ void init_scall()
     for (i = 0; i < 255; i++)
         system_calls[i] = sys_nocall;
 
-    system_calls[1] = sys_exit;
-    system_calls[2] = sys_fork;
+//    system_calls[1] = sys_exit;
+//    system_calls[2] = sys_fork;
     system_calls[3] = sys_read;
     system_calls[4] = sys_write;
     system_calls[5] = sys_open;
     system_calls[6] = sys_close;
-    system_calls[7] = sys_waitpid;
+//    system_calls[7] = sys_waitpid;
     system_calls[10] = sys_unlink;
-    system_calls[11] = sys_execve;
+//    system_calls[11] = sys_execve;
     system_calls[12] = sys_chdir;
     system_calls[19] = sys_lseek;
-    system_calls[20] = sys_getpid;
+//    system_calls[20] = sys_getpid;
     system_calls[38] = sys_rename;
     system_calls[39] = sys_mkdir;
     system_calls[40] = sys_rmdir;
