60830aa41fe09c630d57e2de759088856df039b6
Juan Pablo Bottaro <jpbottaro@gmail.com>
2011-03-04 21:47:51 -0300

    kernel: fix some comments in gdt.c

--- kernel/gdt.c
+++ kernel/gdt.c
@@ -6,7 +6,7 @@ gdt_entry gdt[GDT_COUNT] = {
 		(unsigned short) 0xffff,
 		(unsigned short) 0x0000,
 		(unsigned char)  0x00,
-        (unsigned char)  0x0a,      // Excecute/Read (code)
+        (unsigned char)  0x0a,      // excecute/read (code)
         (unsigned char)  0x01,      // code or data
         (unsigned char)  0x00,      // priv 0
         (unsigned char)  0x01, 
@@ -21,7 +21,7 @@ gdt_entry gdt[GDT_COUNT] = {
 		(unsigned short) 0xffff,
 		(unsigned short) 0,
 		(unsigned char)  0,
-        (unsigned char)  2,         // Read/Write (data)
+        (unsigned char)  2,         // read/write (data)
         (unsigned char)  1,         // code or data
         (unsigned char)  0,         // priv 0
         (unsigned char)  1, 
@@ -33,10 +33,10 @@ gdt_entry gdt[GDT_COUNT] = {
         (unsigned char)  0
 	},
 	(gdt_entry){ // video
-		(unsigned short) 0x0f9f,    // 80x25chars * 2 bytes - 1
+		(unsigned short) 0x0f9f,    // 80x25 chars * 2 bytes - 1
 		(unsigned short) 0x8000,
 		(unsigned char)  0xb,
-        (unsigned char)  2,         // Read/Write (data)
+        (unsigned char)  2,         // read/write (data)
         (unsigned char)  1,         // code or data
         (unsigned char)  0,         // priv 0
         (unsigned char)  1, 
@@ -51,7 +51,7 @@ gdt_entry gdt[GDT_COUNT] = {
 
 gdt_descriptor GDT_DESC = {sizeof(gdt)-1, (unsigned int) &gdt};
 
-unsigned int gdt_free = (unsigned int) &gdt + 0x18;  // donde esta mapeada la memoria de video
+unsigned int gdt_free = (unsigned int) &gdt + 0x18;
 
 unsigned int gdt_free_entry()
 {
