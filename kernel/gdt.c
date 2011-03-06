c236e73eeb9cfdaaa8ec757291b9c0fb43107bf0
Juan Pablo Bottaro <jpbottaro@gmail.com>
2011-03-06 03:38:56 -0300

    kernel: typos in gdt.c

--- kernel/gdt.c
+++ kernel/gdt.c
@@ -19,33 +19,33 @@ gdt_entry gdt[GDT_COUNT] = {
 	},
 	(gdt_entry){ // data
 		(unsigned short) 0xffff,
-		(unsigned short) 0,
-		(unsigned char)  0,
-        (unsigned char)  2,         // read/write (data)
-        (unsigned char)  1,         // code or data
-        (unsigned char)  0,         // priv 0
-        (unsigned char)  1, 
+		(unsigned short) 0x0000,
+		(unsigned char)  0x00,
+        (unsigned char)  0x02,         // read/write (data)
+        (unsigned char)  0x01,         // code or data
+        (unsigned char)  0x00,         // priv 0
+        (unsigned char)  0x01, 
         (unsigned char)  0x0f, 
-        (unsigned char)  0, 
-        (unsigned char)  0, 
-        (unsigned char)  1,
-        (unsigned char)  1,
-        (unsigned char)  0
+        (unsigned char)  0x00, 
+        (unsigned char)  0x00, 
+        (unsigned char)  0x01,
+        (unsigned char)  0x01,
+        (unsigned char)  0x00
 	},
 	(gdt_entry){ // video
 		(unsigned short) 0x0f9f,    // 80x25 chars * 2 bytes - 1
 		(unsigned short) 0x8000,
-		(unsigned char)  0xb,
-        (unsigned char)  2,         // read/write (data)
-        (unsigned char)  1,         // code or data
-        (unsigned char)  0,         // priv 0
-        (unsigned char)  1, 
-        (unsigned char)  0,
-        (unsigned char)  0, 
-        (unsigned char)  0, 
-        (unsigned char)  1,
-        (unsigned char)  0,         // NOT 4 chunks
-        (unsigned char)  0
+		(unsigned char)  0x0b,
+        (unsigned char)  0x02,         // read/write (data)
+        (unsigned char)  0x01,         // code or data
+        (unsigned char)  0x00,         // priv 0
+        (unsigned char)  0x01, 
+        (unsigned char)  0x00,
+        (unsigned char)  0x00, 
+        (unsigned char)  0x00, 
+        (unsigned char)  0x01,
+        (unsigned char)  0x00,         // NOT 4 chunks
+        (unsigned char)  0x00
 	}
 };
 
