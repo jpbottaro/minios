21f2fdc3c487b0d05af7114177ec99b0f63bc1d5
Juan Pablo Bottaro <jpbottaro@gmail.com>
2011-03-12 22:01:29 -0300

    include: add definitions for getdents()

--- include/minikernel/dirent.h
+++ include/minikernel/dirent.h
@@ -5,13 +5,21 @@
 
 #include <sys/types.h>
 
+#define DT_REG  1
+#define DT_DIR  2
+#define DT_FIFO 3
+#define DT_SOCK 4
+#define DT_LNK  5
+#define DT_BLK  6
+#define DT_CHR  7
+
 struct dirent {
     ino_t   d_ino;           /* I-node number */
     off_t   d_off;           /* Offset in directory */
     unsigned short d_reclen; /* Length of this record */
-    char    d_name[1];       /* Null terminated name */
+    char    d_name[256];        /* Null terminated name */
 };
 
-int getdents(int fd, struct dirent *buf, size_t n);
+int getdents(int fd, char *buf, size_t n);
 
 #endif /* _DIRENT_H */
