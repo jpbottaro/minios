07f178775fb18c790087509f7dac79647a478664
Juan Pablo Bottaro <jpbottaro@gmail.com>
2011-03-12 22:08:37 -0300

    build: add dumb script to copy apps into the fs image

---
+++ bin/sh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo mount -o loop fsimage tmp && \
+sudo cp ../apps/cash tmp/bin/ && \
+sudo cp ../apps/lineecho tmp/bin/ && \
+sudo cp ../apps/echo tmp/bin/ && \
+sudo cp ../apps/argc tmp/bin/ && \
+sudo cp ../apps/ls tmp/bin/ && \
+sudo umount tmp
+
