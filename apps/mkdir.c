7edc5c5436a7d26d9218a312ac405948895c85e3
Juan Pablo Bottaro <jpbottaro@gmail.com>
2011-03-15 03:56:33 -0300

    apps: add mkdir and rmdir

---
+++ apps/mkdir.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "misc.h"
+
+#define ERR_ARGC "To few arguments\n"
+#define ERR_MKDIR "Error in mkdir\n"
+
+void main(int argc, const char *argv[])
+{
+    if (argc < 2) {
+        write(STDOUT_FILENO, ERR_ARGC, sizeof(ERR_ARGC));
+        _exit(1);
+    }
+
+    if (mkdir(argv[1], 0) < 0) {
+        write(STDOUT_FILENO, ERR_MKDIR, sizeof(ERR_MKDIR));
+        _exit(1);
+    }
+
+    _exit(0);
+}
---
+++ apps/rmdir.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "misc.h"
+
+#define ERR_ARGC "To few arguments\n"
+#define ERR_RMDIR "Error in rmdir\n"
+
+void main(int argc, const char *argv[])
+{
+    if (argc < 2) {
+        write(STDOUT_FILENO, ERR_ARGC, sizeof(ERR_ARGC));
+        _exit(1);
+    }
+
+    if (rmdir(argv[1]) < 0) {
+        write(STDOUT_FILENO, ERR_RMDIR, sizeof(ERR_RMDIR));
+        _exit(1);
+    }
+
+    _exit(0);
+}
