0ee3b9fca693a2f464a1e6a6be9f992b478f76f6
Juan Pablo Bottaro <jpbottaro@gmail.com>
2011-03-17 18:29:27 -0300

    build: fix fs image, see ISSUES to know why this was needed

--- bin/sh.sh
+++ bin/sh.sh
@@ -6,10 +6,10 @@ sudo cp ../apps/lineecho tmp/bin/ && \
 sudo cp ../apps/echo tmp/bin/ && \
 sudo cp ../apps/argc tmp/bin/ && \
 sudo cp ../apps/ls tmp/bin/ && \
-sudo cp ../apps/cat tmp/bin/ && \
-sudo cp ../apps/cp tmp/bin/ && \
 sudo cp ../apps/rm tmp/bin/ && \
 sudo cp ../apps/mkdir tmp/bin/ && \
 sudo cp ../apps/rmdir tmp/bin/ && \
+sudo cp ../apps/cat tmp/bin/ && \
+sudo cp ../apps/cp tmp/bin/ && \
 sudo umount tmp
 
