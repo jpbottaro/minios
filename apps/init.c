88ac7b23b1dc53f394e91344ef49e1bfa121954e
Juan Pablo Bottaro <jpbottaro@gmail.com>
2011-06-14 00:13:02 -0300

    apps: add init app

--- apps/init.c
+++ apps/init.c
@@ -3,10 +3,23 @@
 #include "misc.h"
 
 #define MAX_BUF 1024
+#define MAX_CONSOLES 3
 
 void main(int argc, char *argv[])
 {
-    char buf[MAX_BUF];
-    int i, len, fd;
+    char tty[] = "/dev/tty0";
+    int i;
 
+    for (i = 0; i < MAX_CONSOLES; ++i) {
+        tty[8] = i + '0';
+        close(STDIN_FILENO);
+        close(STDOUT_FILENO);
+        close(STDERR_FILENO);
+        open(tty, O_RDWR, 0);
+        open(tty, O_RDWR, 0);
+        open(tty, O_RDWR, 0);
+        newprocess("/bin/cash", NULL);
+    }
+
+    _exit(0);
 }
