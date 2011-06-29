853571ec8874b0af7f1721580b62d894d0ec06a7
Juan Pablo Bottaro <jpbottaro@gmail.com>
2011-06-29 03:32:15 -0300

    driver/hdd: add idea for the driver, taken from minix

--- drivers/hdd/hdd.c
+++ drivers/hdd/hdd.c
@@ -1,8 +1,161 @@
+/* HEAVILY based on at_wini driver from minix v3 */
+
 #include <minios/i386.h>
+#include <minios/debug.h>
 #include <minios/debug.h>
 #include <minios/dev.h>
 #include <minios/idt.h>
 #include "hdd.h"
+
+#define ULONG_MAX 4294967295UL
+
+/* Common command block */
+struct command {
+    u8_t	precomp;	/* REG_PRECOMP, etc. */
+    u8_t	count;
+    u8_t	sector;
+    u8_t	cyl_lo;
+    u8_t	cyl_hi;
+    u8_t	ldh;
+    u8_t	command;
+
+    /* The following at for LBA48 */
+    u8_t	count_prev;
+    u8_t	sector_prev;
+    u8_t	cyl_lo_prev;
+    u8_t	cyl_hi_prev;
+};
+
+static struct wini {
+    unsigned state;         /* drive state: deaf, initialized, dead */
+    unsigned short w_status;    /* device status register */
+    unsigned lcylinders;        /* logical number of cylinders (BIOS) */
+    unsigned lheads;        /* logical number of heads */
+    unsigned lsectors;      /* logical number of sectors per track */
+    unsigned pcylinders;    /* physical number of cylinders (translated) */
+    unsigned pheads;        /* physical number of heads */
+    unsigned psectors;      /* physical number of sectors per track */
+    unsigned open_ct;       /* in-use count */
+    unsigned long size;     /* total size */
+} wn;
+
+u8_t tmp_buf[SECTOR_SIZE];
+
+#define id_byte(n)	(&tmp_buf[2 * (n)])
+#define id_word(n)	(((u16_t) id_byte(n)[0] <<  0) \
+        | ((u16_t) id_byte(n)[1] <<  8))
+#define id_longword(n)	(((u32_t) id_byte(n)[0] <<  0) \
+        | ((u32_t) id_byte(n)[1] <<  8) \
+        | ((u32_t) id_byte(n)[2] << 16) \
+        | ((u32_t) id_byte(n)[3] << 24))
+
+int w_waitfor(mask, value)
+{
+    int s;
+
+    /* wtf polling ! we should have a timeout here or smth */
+    while ( ((s = inb(HDD_CMD + REG_STATUS)) & mask) != value)
+        ;
+    wn.w_status = s;
+
+    return 1;
+}
+
+int com_out(struct command *cmd)
+{
+    w_waitfor(STATUS_BSY, 0);
+
+    /* Select drive. */
+    outb(HDD_CMD + REG_LDH, cmd->ldh);
+
+    w_waitfor(STATUS_BSY, 0);
+    if (wn.w_status == 0)
+        debug_panic("hard disk does not exist!!");
+
+    wn.w_status = STATUS_ADMBSY;
+    outb(HDD_CTL + REG_CTL, wn.pheads >= 8 ? CTL_EIGHTHEADS : 0);
+    outb(HDD_CMD + REG_PRECOMP, cmd->precomp);
+    outb(HDD_CMD + REG_COUNT, cmd->count);
+    outb(HDD_CMD + REG_SECTOR, cmd->sector);
+    outb(HDD_CMD + REG_CYL_LO, cmd->cyl_lo);
+    outb(HDD_CMD + REG_CYL_HI, cmd->cyl_hi);
+    outb(HDD_CMD + REG_COMMAND, cmd->command);
+
+    return 0;
+}
+
+int at_intr_wait()
+{
+    /* Wait for an interrupt */
+    int r;
+
+    w_waitfor(STATUS_BSY, 0);
+    if ((wn.w_status & (STATUS_BSY | STATUS_WF | STATUS_ERR)) == 0) {
+        r = 0;
+    } else {
+        debug_panic("at_intr_wait: wtf error");
+    }
+    wn.w_status |= STATUS_ADMBSY;   /* assume still busy with I/O */
+    return r;
+}
+
+int com_simple(struct command *cmd)
+{
+    /* A controller command, only one interrupt and no data-out phase. */
+    int r;
+
+    if ((r = com_out(cmd)) == 0)
+        r = at_intr_wait();
+
+    return r;
+}
+
+int get_data(int port, u8_t *buf, int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        buf[i] = inb(port);
+    return 0;
+}
+
+static int identify()
+{
+    int size, r;
+    struct command cmd;
+
+    cmd.ldh     = LDH_DEFAULT;
+    cmd.command = ATA_IDENTIFY;
+    r = com_simple(&cmd);
+
+    if (r == 0 && w_waitfor(STATUS_DRQ, STATUS_DRQ) &&
+            !(wn.w_status & (STATUS_ERR|STATUS_WF))) {
+
+        /* Device information. */
+        if (get_data(HDD_CMD + REG_DATA, tmp_buf, SECTOR_SIZE) != 0)
+            debug_panic("identify: call to get_data() failed");
+
+        wn.pcylinders = id_word(1);
+        wn.pheads = id_word(3);
+        wn.psectors = id_word(6);
+        size = (u32_t) wn.pcylinders * wn.pheads * wn.psectors;
+
+        /* assume LBA */
+        if (id_longword(102)) {
+            /* If no. of sectors doesn't fit in 32 bits,
+             * trunacte to this. So it's LBA32 for now.
+             * This can still address devices up to 2TB
+             * though.
+             */
+            size = ULONG_MAX;
+        } else {
+            /* Actual number of sectors fits in 32 bits. */
+            size = id_longword(100);
+        }
+        wn.size = size;
+    }
+
+    return 0;
+}
 
 size_t hdd_read(struct file_s *flip, char *buf, size_t n)
 {
@@ -25,16 +178,6 @@ static struct file_operations_s ops = {
     .flush = hdd_flush
 };
 
-static int identify()
-{
-    return 0;
-}
-
-static int io_test()
-{
-    return 0;
-}
-
 void hdd_handler()
 {
 
@@ -49,10 +192,6 @@ void hdd_init()
     /* Try to identify the device. */
     if (identify() != 0)
         debug_panic("hdd_init(): cannot find hdd");
-
-    /* Do a test transaction */
-    if (io_test() != 0)
-        debug_panic("hdd_init(): failed test");
 
     /* make char device in /dev */
     //fs_make_dev("hdd", I_BLOCK, DEV_HDD, 0);
--- drivers/hdd/hdd.h
+++ drivers/hdd/hdd.h
@@ -4,6 +4,72 @@
 #include <minios/dev.h>
 #include <sys/types.h>
 
+#define HDD_CMD 0x1F0
+#define HDD_CTL 0x3F6
+
+#define REG_DATA	    0	/* data register (offset from the base reg.) */
+#define REG_PRECOMP	    1	/* start of write precompensation */
+#define REG_COUNT	    2	/* sectors to transfer */
+#define REG_SECTOR	    3	/* sector number */
+#define REG_CYL_LO	    4	/* low byte of cylinder number */
+#define REG_CYL_HI	    5	/* high byte of cylinder number */
+#define REG_LDH		    6	/* lba, drive and head */
+#define   LDH_DEFAULT		0xA0	/* ECC enable, 512 bytes per sector */
+
+/* Read only registers */
+#define REG_STATUS	    7	/* status */
+#define   STATUS_BSY		0x80	/* controller busy */
+#define	  STATUS_RDY		0x40	/* drive ready */
+#define	  STATUS_WF		0x20	/* write fault */
+#define	  STATUS_SC		0x10	/* seek complete (obsolete) */
+#define	  STATUS_DRQ		0x08	/* data transfer request */
+#define	  STATUS_CRD		0x04	/* corrected data */
+#define	  STATUS_IDX		0x02	/* index pulse */
+#define	  STATUS_ERR		0x01	/* error */
+#define	  STATUS_ADMBSY	       0x100	/* administratively busy (software) */
+#define REG_ERROR	    1	/* error code */
+#define	  ERROR_BB		0x80	/* bad block */
+#define	  ERROR_ECC		0x40	/* bad ecc bytes */
+#define	  ERROR_ID		0x10	/* id not found */
+#define	  ERROR_AC		0x04	/* aborted command */
+#define	  ERROR_TK		0x02	/* track zero error */
+#define	  ERROR_DM		0x01	/* no data address mark */
+
+/* Write only registers */
+#define REG_COMMAND	    7	/* command */
+#define   CMD_IDLE		0x00	/* for w_command: drive idle */
+#define   CMD_RECALIBRATE	0x10	/* recalibrate drive */
+#define   CMD_READ		0x20	/* read data */
+#define   CMD_READ_EXT		0x24	/* read data (LBA48 addressed) */
+#define   CMD_READ_DMA_EXT	0x25	/* read data using DMA (w/ LBA48) */
+#define   CMD_WRITE		0x30	/* write data */
+#define	  CMD_WRITE_EXT		0x34	/* write data (LBA48 addressed) */
+#define   CMD_WRITE_DMA_EXT	0x35	/* write data using DMA (w/ LBA48) */
+#define   CMD_READVERIFY	0x40	/* read verify */
+#define   CMD_FORMAT		0x50	/* format track */
+#define   CMD_SEEK		0x70	/* seek cylinder */
+#define   CMD_DIAG		0x90	/* execute device diagnostics */
+#define   CMD_SPECIFY		0x91	/* specify parameters */
+#define   CMD_READ_DMA		0xC8	/* read data using DMA */
+#define   CMD_WRITE_DMA		0xCA	/* write data using DMA */
+#define   CMD_FLUSH_CACHE	0xE7	/* flush the write cache */
+#define   ATA_IDENTIFY		0xEC	/* identify drive */
+/* #define REG_CTL		0x206	*/ /* control register */
+#define REG_CTL		0	/* control register */
+#define   CTL_NORETRY		0x80	/* disable access retry */
+#define   CTL_NOECC		0x40	/* disable ecc retry */
+#define   CTL_EIGHTHEADS	0x08	/* more than eight heads */
+#define   CTL_RESET		0x04	/* reset controller */
+#define   CTL_INTDISABLE	0x02	/* disable interrupts */
+#define REG_CTL_ALTSTAT 0	/* alternate status register */
+
+#define LBA48_CHECK_SIZE	0x0f000000
+#define LBA_MAX_SIZE		0x0fffffff	/* Highest sector size for reg LBA */
+
+#define   CTL_EIGHTHEADS	0x08	/* more than eight heads */
+
+#define SECTOR_SIZE 512
+
 void hdd_init();
 
 #endif
