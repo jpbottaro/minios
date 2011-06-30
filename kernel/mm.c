f73dca02e81b7b0033769ebc7afbed1f26de44c2
Juan Pablo Bottaro <jpbottaro@gmail.com>
2011-06-30 15:46:12 -0300

    kernel: memcpy is (to, from), not (from, to) ...

--- kernel/mm.c
+++ kernel/mm.c
@@ -125,7 +125,7 @@ mm_page *mm_dir_cpy(mm_page *dir)
     for (d = dir; d < end; d++, dirbase++) {
         if (d->attr & MM_ATTR_P) {
             tablebase = (mm_page *) mm_mem_alloc();
-            mymemcpy((char *) (d->base << 12), (char *) tablebase, PAGE_SIZE);
+            mymemcpy((char *) tablebase, (char *) (d->base << 12), PAGE_SIZE);
             *dirbase = make_mm_entry_addr(tablebase, d->attr);
         } else {
             *dirbase = make_mm_entry_addr(0, 0);
