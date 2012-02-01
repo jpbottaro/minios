fdced5262466532d204b9913c0a53f2caa8d72ee
Juan Pablo Bottaro <jpbottaro@gmail.com>
2012-02-01 10:56:08 -0300

    driver/hdd: Make ATA_TO_SECTOR() macro safer

--- drivers/hdd/hdd.h
+++ drivers/hdd/hdd.h
@@ -103,7 +103,7 @@
 #define ATA_WRITE      0x01
 
 #define ATA_SECTOR_SIZE 512
-#define ATA_TO_SECTOR(x) (x >> 9)
+#define ATA_TO_SECTOR(x) ((x) >> 9)
 #define ATA_USE_TRANSFER(x) (sem_wait(&((x)->sem_transfer)))
 #define ATA_FREE_TRANSFER(x) (sem_signal(&((x)->sem_transfer)))
 #define ATA_DELAY() do { iowait();iowait();iowait();iowait();iowait(); } while(0)
